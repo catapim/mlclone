a8a7e55bd9f4f0bc9283b36f71bce04fbb89a9be
catalina <catalinapimentel@gmail.com>
2019-08-23 20:22:12 -0400

    y isnt it working

--- src/App.js
+++ src/App.js
@@ -2,41 +2,8 @@ import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
 import './App.scss';
 import axios from 'axios';
-import List from '../components/List';
+// import List from '../components/List';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      //se pone un query para consultar api
-      query: "iphone",
-      items: []
-    }
-    this.handleSearch = this.handleSearch.bind(this);
-    this.handleSearchInput = this.handleSearchInput.bind(this);
-    // this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  //se hace post hacia la api con el query iphone
-  handleSearch(){
-      axios.post('https://api.mercadolibre.com/sites/MLC/search?q=', this.state.query)
-        .then(response => {
-          console.info(response);
-          console.info(response.data);
-          if (response.status === 200) {
-            this.setState({message: 'busqueda OK', data: response.data })
-          }
-        });
-        console.log('submit');
-      }
-
-      handleSearchInput(){
-        this.setState({product: event.target.value})
-
-      }
-      handleSubmit(e){
-        e.preventDefault();      }
   /*
     Actividad Nº 2
 
@@ -49,6 +16,41 @@ class App extends Component {
     - Cada fila debe contener imagen del producto, titulo y precio
     - Agregar dentro del .nav-header el logo (https://http2.mlstatic.com/ui/navigation/4.5.0/mercadolibre/logo__large_plus@2x.png)
   */
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      //se pone un query para consultar api
+      query: "iphone",
+      items: []
+    }
+    this.handleSearch = this.handleSearch.bind(this);
+    this.handleSearchInput = this.handleSearchInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  //se hace post hacia la api con el query iphone
+  handleSearch(){
+      axios.post('https://api.mercadolibre.com/sites/MLC/search?q=', this.state.query)
+        .then(response => {
+          console.info(response);
+          console.info(response.data);
+          if (response.status == 200) {
+            this.setState({message: 'busqueda OK', data: response.data })
+          }
+        });
+        console.log('submit');
+      }
+
+      handleSearchInput(){
+        this.setState({product: event.target.value})
+
+      }
+      handleSubmit(e){
+        e.preventDefault();      
+      }
 
   render() {
     const {items} = this.state;
